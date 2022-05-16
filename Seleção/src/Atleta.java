245a2237d3a5d22144ace73077d79185f3184ab0
emilly-vitoria <emilly-vitoria@test.com>
2022-05-16 10:57:36 -0300

    Desenvolvimento

---
+++ Seleção/src/Atleta.java
@@ -0,0 +1,14 @@
+
+public class Atleta {
+
+	public double peso;
+	public double altura;
+	
+	public Atleta() {
+		this.peso = 1.75;
+		this.altura = 80.0;
+	}
+	public Atleta(double peso) {
+		this.peso = peso;
+	}
+}
---
+++ Seleção/src/TesteAtleta.java
@@ -0,0 +1,18 @@
+
+public class TesteAtleta {
+
+	public static void main(String[] args) {
+		
+		Atleta atleta = new Atleta();
+		atleta.altura = 1.75;
+		atleta.peso = 80;
+		
+		
+		
+		
+		
+		//imc = atleta.peso / (atleta.altura * atleta.altura);
+		
+	}
+}
+
